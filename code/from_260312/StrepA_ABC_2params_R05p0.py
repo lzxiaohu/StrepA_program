813e13961425f4288a42911295f4edbda0e614f3
Ubuntu <ubuntu@strepa-modeling.novalocal>
2026-04-29 03:38:20 +0000

    update experimental for alpha=7.0

--- code/from_260312/StrepA_ABC_2params_R05p0.py
+++ code/from_260312/StrepA_ABC_2params_R05p0.py
@@ -31,7 +31,7 @@ omega = 0.2     # immunity cross strains: 0.1
 x = 10.0        #
 Cperweek = 34.53    #
 Nagents = 1000      # number of agents
-alpha = 0.007        # migration rate: 3.0
+alpha = 7.0        # migration rate: 3.0
 AgeDeath = 71.0     # life expectancy
 # R0: updated parameter (Basic reproductive number)
 # Sigma: updated parameter ()
@@ -238,9 +238,9 @@ def select_epsilon_3params(R0_range, sigma_range, Dimmunity_range, s_obs, scale,
     for ii in range(n_pilot):
         R0_sel, sigma_sel, Dimmunity_sel = prior_value_3params(R0_range, sigma_range, Dimmunity_range, rng)
         y_sim = simulate_prevalence_v5_numba([R0_sel, sigma_sel, Dimmunity_sel*52.14], fixed_params, core_params_num, seed)
-        print("y_sim:", y_sim)
+        # print("y_sim:", y_sim)
         s_sim = summary_stats(y_sim, scale=scale)
-        print("s_sim:", s_sim)
+        # print("s_sim:", s_sim)
         tempt = discrepancy(s_sim, s_obs, scale, weights=weights)
         if np.isnan(tempt):
             # print("this is nan")
@@ -267,9 +267,9 @@ def select_epsilon_2params(R0_range, sigma_range, s_obs, scale, n_pilot=5000, qu
     for ii in range(n_pilot):
         R0_sel, sigma_sel = prior_value_2params(R0_range, sigma_range, rng)
         y_sim = simulate_prevalence_v5_numba([R0_sel, sigma_sel], fixed_params, core_params_num, seed)
-        print("y_sim:", y_sim)
+        # print("y_sim:", y_sim)
         s_sim = summary_stats(y_sim, scale=scale)
-        print("s_sim:", s_sim)
+        # print("s_sim:", s_sim)
         tempt = discrepancy(s_sim, s_obs, scale, weights=weights)
         if np.isnan(tempt):
             # print("this is nan")
@@ -334,9 +334,9 @@ def abc_reject_2params(R0_range, sigma_range, core_params_num, s_obs, scale, eps
         trials += 1
         R0_sel, sigma_sel = prior_value_2params(R0_range, sigma_range, rng)
         y_sim = simulate_prevalence_v5_numba([R0_sel, sigma_sel], fixed_params, core_params_num, seed)
-        print("y_sim", y_sim)
+        # print("y_sim", y_sim)
         s_sim = summary_stats(y_sim, scale=scale)
-        print("s_sim", s_sim)
+        # print("s_sim", s_sim)
         dist = discrepancy(s_sim, s_obs, scale, weights=weights)
 
         if dist <= eps + 0.000001:
@@ -371,7 +371,7 @@ if core_params_num == 2:
     # print("dists: ", len(pilots))
 
     post, dists_acc, trials, ss = abc_reject_2params(R0_range, sigma_range, core_params_num, s_obs_v5_numba,
-                                                     scale, eps, n_accept=8000, max_trials=2_000_000,
+                                                     scale, eps, n_accept=5000, max_trials=2_000_000,
                                                      seed=123)
     logging.info(f"Accepted: {len(post)}; Trials: {trials}; Acceptance rate: {len(post) / trials}")
     # print("Accepted: ", len(post), "Trials: ", trials, "Acceptance rate: ", len(post) / trials)
@@ -396,7 +396,7 @@ elif core_params_num == 3:
     # print("eps: ", eps)
     # print("dists: ", len(pilots))
     post, dists_acc, trials, ss = abc_reject_3params(R0_range, sigma_range, Dimmunity_range, core_params_num,
-                                                     s_obs_v5_numba, scale, eps, n_accept=8000,
+                                                     s_obs_v5_numba, scale, eps, n_accept=5000,
                                                      max_trials=2_000_000, seed=123)
     logging.info(f"Accepted: {len(post)}; Trials: {trials}; Acceptance rate: {len(post) / trials}")
     # print("Accepted: ", len(post), "Trials: ", trials, "Acceptance rate: ", len(post) / trials)
