e73d0805d179cf6f16365beac1ad86a791c6fce2
Ubuntu <ubuntu@joseph-modeling.novalocal>
2026-05-06 04:26:30 +0000

    partial experimental results

--- code/from_260312/StrepA_ABC_2params_R02p0.py
+++ code/from_260312/StrepA_ABC_2params_R02p0.py
@@ -28,7 +28,7 @@ omega = 0.2     # immunity cross strains: 0.1
 x = 10.0        #
 Cperweek = 34.53    #
 Nagents = 1000      # number of agents
-alpha = 0.007        # migration rate: 3.0
+alpha = 7.0        # migration rate: 3.0
 AgeDeath = 71.0     # life expectancy
 # R0: updated parameter (Basic reproductive number)
 # Sigma: updated parameter ()
@@ -343,7 +343,7 @@ if core_params_num == 2:
     print("dists: ", len(pilots))
 
     post, dists_acc, trials, ss = abc_reject_2params(R0_range, sigma_range, core_params_num, s_obs_v5_numba,
-                                                     scale, eps, n_accept=4000, max_trials=2_000_000,
+                                                     scale, eps, n_accept=8000, max_trials=2_000_000,
                                                      seed=123)
     print("Accepted: ", len(post), "Trials: ", trials, "Acceptance rate: ", len(post) / trials)
 
@@ -364,7 +364,7 @@ elif core_params_num == 3:
     print("eps: ", eps)
     print("dists: ", len(pilots))
     post, dists_acc, trials, ss = abc_reject_3params(R0_range, sigma_range, Dimmunity_range, core_params_num,
-                                                     s_obs_v5_numba, scale, eps, n_accept=4000,
+                                                     s_obs_v5_numba, scale, eps, n_accept=8000,
                                                      max_trials=2_000_000, seed=123)
     print("Accepted: ", len(post), "Trials: ", trials, "Acceptance rate: ", len(post) / trials)
 
